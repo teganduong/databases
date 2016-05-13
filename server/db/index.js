22d48c7d121344e3fb382c8ff691553381f14b96
Tegan Duong <teganduong@gmail.com>
2016-05-13 12:24:37 -0700

    Add client code and setup database connection

--- server/db/index.js
+++ server/db/index.js
@@ -4,4 +4,19 @@ var mysql = require('mysql');
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
 
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '23',
+  database: 'chat'
+});
 
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+  console.log('connected to database!');
+});
+
+exports.db = connection;
--- server/schema.sql
+++ server/schema.sql
@@ -1,6 +1,9 @@
+DROP DATABASE IF EXISTS chat;
 CREATE DATABASE chat;
 
 USE chat;
+
+DROP TABLE IF EXISTS messages;
 
 CREATE TABLE messages (
   id INTEGER NOT NULL AUTO_INCREMENT,
@@ -11,6 +14,8 @@ CREATE TABLE messages (
 );
 
 /* Create other tables and define schemas for them here! */
+DROP TABLE IF EXISTS users;
+
 CREATE TABLE users (
   id INTEGER NOT NULL AUTO_INCREMENT,
   username VARCHAR(20) NOT NULL,
