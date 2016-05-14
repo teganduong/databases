3354b6295ce1e9b0bfa6efd4c98ade34d43a3d99
Tegan Duong <teganduong@gmail.com>
2016-05-13 18:56:27 -0700

    Modify test specs

--- server/spec/server-spec.js
+++ server/spec/server-spec.js
@@ -57,7 +57,7 @@ describe('Persistent Node Chat Server', function() {
           expect(results.length).to.equal(1);
 
           // TODO: If you don't have a column named text, change this test.
-          expect(results[0].text).to.equal('In mercy\'s name, three days is all I need.');
+          expect(results[0].message).to.equal('In mercy\'s name, three days is all I need.');
 
           done();
         });
@@ -67,8 +67,8 @@ describe('Persistent Node Chat Server', function() {
 
   it('Should output all messages from the DB', function(done) {
     // Let's insert a message into the db
-       var queryString = "";
-       var queryArgs = [];
+       var queryString = 'INSERT INTO messages (userid, message, roomname) VALUES (?, ?, ?)';
+       var queryArgs = [1, 'Hello', 'main'];
     // TODO - The exact query string and query args to use
     // here depend on the schema you design, so I'll leave
     // them up to you. */
@@ -80,7 +80,7 @@ describe('Persistent Node Chat Server', function() {
       // the message we just inserted:
       request('http://127.0.0.1:3000/classes/messages', function(error, response, body) {
         var messageLog = JSON.parse(body);
-        expect(messageLog[0].text).to.equal('Men like you can never change!');
+        expect(messageLog[0].message).to.equal('Hello');
         expect(messageLog[0].roomname).to.equal('main');
         done();
       });
