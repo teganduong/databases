9b88c55aa57e2bc6ef574381c3164039c2985d9d
Tegan Duong <teganduong@gmail.com>
2016-05-13 18:51:49 -0700

    Implement messages and users models in server

--- server/models/index.js
+++ server/models/index.js
@@ -1,15 +1,56 @@
 var db = require('../db');
 
+// models are the files where you interact w/ the db
+
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+    // a function which produces all the messages
+    get: function (callback) {
+      var queryString = 'SELECT * FROM messages';
+      db.query(queryString, function(err, results) {
+        if (err) {
+          console.error('Error in retrieving messages: ', err);
+        }
+        callback(results);
+      });
+    }, 
+
+    // a function which can be used to insert a message into the database
+    post: function () {
+      var queryString = 'INSERT INTO messages (userid, msg, roomname) VALUES (?, ?, ?)';
+      var queryArgs = [1, 'Hello', 'main'];
+      db.query(queryString, queryArgs, function(err, response) {
+        if (err) {
+          console.error('Error in posting message to database: ', err);
+        }
+        console.log('message successfully inserted into database!');
+      });
+    } 
   },
 
   users: {
-    // Ditto as above.
-    get: function () {},
-    post: function () {}
+    
+    get: function (callback) {
+      var queryString = 'SELECT * FROM users';
+      db.query(queryString, function(err, results) {
+        if (err) {
+          console.error('Error in retrieving users: ', err);
+        }
+        console.log('all users: ', results);
+        callback(results);
+      });
+    },
+
+    post: function () {
+      var queryString = 'INSERT INTO users (username) VALUES (?)';
+      var queryArgs = 'tegan';
+      db.query(queryString, queryArgs, function(err, response) {
+        if (err) {
+          console.error('Error in posting user to database: ', err);
+        }
+        console.log('user successfully inserted into database!');
+      });
+    }
   }
 };
 
